3b93149ff7f15cac8346c0f5af685ee2e1579cea
Kimbyul88 <byul88byul@gmail.com>
2024-01-30 11:00:26 +0900

    effect 배우기 까지 완료
    -index.js에서 strict mode 지움! (두번 렌더링 방지)

--- real-app/src/App.js
+++ real-app/src/App.js
@@ -1,12 +1,28 @@
-import Button from "./Button";
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+  };
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
-
 export default App;
