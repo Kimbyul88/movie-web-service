2715f60661fc6a00c477711488c80d929ecee8ab
Kimbyul88 <byul88byul@gmail.com>
2024-01-31 15:04:47 +0900

    css 시도..
    ..

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
+  const [moviedetail, setMovie] = useState();
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
+    console.log(moviedetail);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div
+      className={styles.yeah}
+      style={
+        {
+          // backgroundImage: `url(${moviedetail.background_image})`,
+        }
+      }
+    >
+      <div>
+        {/* <h1 className={styles.title}>{moviedetail.title}</h1>
+        <img
+          className={styles.cover_image}
+          alt={moviedetail.title}
+          src={moviedetail.large_cover_image}
+        ></img> */}
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
